a5874b5fd7984a4ac253510a8054ab311dbc40de
JackieGu-Company <jackieggu@gmail.com>
2021-01-27 17:49:47 +0800

    新增自动依赖注入与请求Mapping注解, 并新增测试接口

--- src/main/java/cn/jackiegu/spring/study/controller/TestController.java
+++ src/main/java/cn/jackiegu/spring/study/controller/TestController.java
@@ -2,10 +2,29 @@ package cn.jackiegu.spring.study.controller;
 
 import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
+import cn.jackiegu.spring.study.annotation.Autowired;
 import cn.jackiegu.spring.study.annotation.Controller;
+import cn.jackiegu.spring.study.annotation.RequestMapping;
+import cn.jackiegu.spring.study.service.TestService;
 
 @Controller
+@RequestMapping("test")
 public class TestController {
 
     private static final Log LOGGER = LogFactory.get();
+
+    @Autowired
+    private TestService testService;
+
+    @RequestMapping("hello")
+    public String hello() {
+        LOGGER.info("Hello Spring MVC");
+        return "Hello Spring MVC";
+    }
+
+    @RequestMapping("sayHi")
+    public String sayHi(String name) {
+        LOGGER.info("Hi " + name);
+        return "Hi " + name;
+    }
 }
