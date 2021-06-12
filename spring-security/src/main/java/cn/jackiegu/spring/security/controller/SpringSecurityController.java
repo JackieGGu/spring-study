26e5f3350cad0b2d80588d9f272a011a83d252ad
JackieGu-Book <jackieggu@gmail.com>
2021-06-12 17:32:11 +0800

    升级Spring Security配置

--- spring-security/src/main/java/cn/jackiegu/spring/security/controller/SpringSecurityController.java
+++ spring-security/src/main/java/cn/jackiegu/spring/security/controller/SpringSecurityController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("security")
 public class SpringSecurityController {
 
-    @RequestMapping("dashboard")
+    @RequestMapping("hello")
     public String dashboard() {
-        return "dashboard";
+        return "Hello Spring Security";
     }
 }
