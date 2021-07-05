f1944634091aa5ecfb1956acece9146bcffde250
JackieGu-Company <jackieggu@gmail.com>
2021-07-05 18:11:56 +0800

    新增静态资源与相关配置

--- spring-security-boot/src/main/java/cn/jackiegu/spring/security/boot/controller/SpringSecurityBootController.java
+++ spring-security-boot/src/main/java/cn/jackiegu/spring/security/boot/controller/SpringSecurityBootController.java
@@ -2,6 +2,7 @@ package cn.jackiegu.spring.security.boot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Spring Security 测试接口
@@ -12,6 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("security/boot")
 public class SpringSecurityBootController {
+
+    /**
+     * 自定义登录页
+     */
+    @RequestMapping("login")
+    public ModelAndView login() {
+        return new ModelAndView("custom-login");
+    }
 
     @RequestMapping("hello")
     public String hello() {
